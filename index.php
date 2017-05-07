855462a10ffbbc53966d27dfd2f51cda36bf18aa
Taufan <getaufan@gmail.com>
2017-05-08 01:36:08 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!--
+Susun Kata Beta 1.0
+Developed by getaufan
+-->
+
+<!-- configration -->
+<?php include "config/config.php" ?>
+
+<!-- header -->
+<?php include "view/header.php" ?>
+
+<!-- content -->
+<?php include "view/body.php" ?>
+
+<!-- footer -->
+<?php include "view/footer.php" ?>
