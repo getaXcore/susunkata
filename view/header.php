edf15a349c8ad007d839e4909f7a84578d5c1be9
Taufan <getaufan@gmail.com>
2017-05-08 12:09:05 +0700

    Update header.php

--- view/header.php
+++ view/header.php
@@ -1,9 +1,60 @@
 <html>
 <head>
 	<title><?php echo TITLE;?></title>
+	<meta  name="viewport" content="width=device-width,initial-scale=1.0">
 	<script type="text/javascript" src="js/jquery-1.12.4.js"></script>
 	<script type="text/javascript" src="js/mainjs.js"></script>
 	<link rel="stylesheet" href="css/style.css">
+	<style type="text/css">
+		@media only screen and (orientation:portrait) {
+		    #wordBox {
+		        background-color: rgba(0, 188, 212, 0.06);
+		        width: 60%;
+		        height: 20%;
+		        border-bottom: none;
+		        border-top: none;
+		        border-left: none;
+		        border-right: none;
+		        text-align: center;
+		        -webkit-padding-before: 11%;
+		        font-size: xx-large;
+		        font-weight: bolder;
+		        color: rgb(60, 197, 192);
+		    }
+		    input#words_answer {
+			    border-top: none;
+			    border-left: none;
+			    border-right: none;
+			    border-bottom: solid 2px rgb(240, 251, 253);
+			    width: 60%;
+			}
+			input#words_submit {
+			    background-color: aliceblue;
+			    border-bottom: none;
+			    border-top: none;
+			    border-left: none;
+			    border-right: none;
+			    width: 60%;
+			    height: 6%;
+			}
+			.wordSoal {
+			    font-style: italic;
+			    padding-bottom: 1%;
+			    font-size: small;
+			}
+			.wordPoin {
+			    padding-bottom: 1%;
+			    font-style: italic;
+			    font-size: small;
+			}
+			.wordCom {
+			    padding-top: 3%;
+			    padding-bottom: 2%;
+			    font-style: italic;
+			    font-size: small;
+			}
+		}
+	</style>
 </head>
 <body>
 <!-- add here -->
